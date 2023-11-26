ed572fe7c5652ab408bf07b11d52a17b21a0125f
Rangel Luiz Bombonatto <rangel-16@hotmail.com>
2023-11-26 19:44:14 -0300

    subindo alteracoes

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -41,7 +41,7 @@ describe('Central de Atendimento ao Cliente TAT', () => {
         cy.get('#firstName').type('Rangel Luiz')
         cy.get('#lastName').type('Bombonatto')
         cy.get('#email').type('rangel@exemplo.com')
-        cy.get('#phone-checkbox').click()
+        cy.get('#phone-checkbox').check()
         cy.get('#open-text-area').type('teste')
         cy.contains('button','Enviar').click()
         
@@ -93,5 +93,78 @@ describe('Central de Atendimento ao Cliente TAT', () => {
         cy.get('#product')
             .select('mentoria')
             .should('have.value', 'mentoria')
+    })
+
+    it('seleciona um produto (Blog) pelo seu indice', () => {
+        cy.get('#product')
+            .select(1)
+            .should('have.value', 'blog')
+    })
+
+    it('marca o tipo de atendimento "Feedback"', () => {
+        cy.get('input[type="radio"][value="feedback"]')
+            .check()
+            .should('have.value','feedback')
+    })
+
+    it('marca cada tipo de atendimento', () => {
+        cy.get('input[type="radio"]')
+            .should('have.length', 3)
+            .each(($radio) => {
+                cy.wrap($radio).check()
+                cy.wrap($radio).should('be.checked')
+            })
+    })
+
+    it('marca ambos os checkboxs, depois desmarca o ultimo', () => {
+        cy.get('input[type="checkbox"]')
+            .check()
+            .should('be.checked')
+            .last()
+            .uncheck()
+            .should('not.be.checked')
+            
+    })
+
+    it('selecione um arquivo na pasta fixture', () => {
+        cy.get('input[type="file"]#file-upload')
+            .should('not.have.value')
+            .selectFile('./cypress/fixtures/example.json')
+            .should(($input) => {
+                expect($input[0].files[0].name).to.equal('example.json')
+            })
+    })
+
+    // simulando como se tivesse arrastando o arquivo para o componente
+    it('selecione um arquivo simulando um drag-and-drop', () => {
+        cy.get('input[type="file"]#file-upload')
+            .should('not.have.value')
+            .selectFile('./cypress/fixtures/example.json', { action: 'drag-drop'})
+            .should(($input) => {
+                expect($input[0].files[0].name).to.equal('example.json')
+            })
+    })
+
+    it('seleciona um arquivo utilizando uma fixture para qual foi dada um alias', () => {
+        cy.fixture('example.json').as('samplefile')
+        cy.get('input[type="file"]')
+            .selectFile('@samplefile')
+            .should(($input) => {
+                expect($input[0].files[0].name).to.equal('example.json')
+            })
+    })
+
+    it('verifica que a politica de privacidade abre em outra abra sem necessidade de clique', () => {
+        // todo link que abre em uma nova aba vai ter a propriedade target com o valor _blank
+        cy.get('#privacy a').should('have.attr','target','_blank')
+    })
+
+    it('acessa a página da política de privacidade removendo o target e então clicando no link', () => {
+        // o cypress não trabalha com abas diferentes, removento a propriedade será aberto na mesma aba o link
+        cy.get('#privacy a')
+            .invoke('removeAttr', 'target')
+            .click()
+
+        cy.contains('Talking About Testing').should('be.visible')
     })
 })
